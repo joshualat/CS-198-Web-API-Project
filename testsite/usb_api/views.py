551cdb8d9132f4e854c099d3460d5295c0df0bfa
josh <josh@bixly.com>
2011-09-24 00:37:30 +0800

    Created ConnectedTools

--- testsite/usb_api/views.py
+++ testsite/usb_api/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):    
     """Introduction, Tutorials"""
     return HttpResponse("Home")
+
+@csrf_exempt
+def test(request):
+    if request.method == 'POST':
+        message = 'POST:' + str(request.POST.get('message',None))
+    elif request.method == 'GET':
+        message = 'GET:' + str(request.GET.get('message',None))
+    return HttpResponse(message+" :)")
---
+++ usb_programs/lib/ConnectTools.py
@@ -0,0 +1,32 @@
+import urllib
+import urllib2
+import webbrowser
+
+# ConnectTools Class
+
+class ConnectTools(object):
+
+    """
+    ConnectTools class
+
+    Usable functions:
+        request_get
+        request_post
+        browser_open
+    """
+
+    @classmethod
+    def request_get(cls,url,params={}):
+        data = urllib.urlencode(params)
+        return urllib2.urlopen(url + "?" + data).read()
+
+    @classmethod
+    def request_post(cls,url,params={}):
+        data = urllib.urlencode(params)
+        return urllib2.urlopen(url, data).read()
+
+    @classmethod
+    def browser_open(cls,url,params={}):
+        data = urllib.urlencode(params)
+        return webbrowser.open(url + "?" + data)
+
