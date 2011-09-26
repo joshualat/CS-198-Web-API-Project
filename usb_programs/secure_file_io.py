12cdeb966663fb828c69bad658ae14d312fc6033
josh <josh@bixly.com>
2011-09-26 09:29:02 +0800

    code documentation for secure_web_connection.py and secure_file_io.py

--- usb_programs/secure_file_io.py
+++ usb_programs/secure_file_io.py
@@ -11,16 +11,20 @@ class SecureFileIO(object):
         load_web_data
         update_url_data
         load_url_data
+        save_usb_data
+        load_usb_data
 
     """
 
     @classmethod
     def save_web_data(cls,web_data):
+        """saves the web data to web.data file"""
         web_data = SecTools.serialize(web_data)
         ConsoleTools.file_write('box/web.data',web_data)
 
     @classmethod
     def load_web_data(cls):        
+        """loads the web data from web.data file"""
         web_data = ConsoleTools.file_read('box/web.data')
         if web_data != None:
             web_data = SecTools.deserialize(web_data)
@@ -30,6 +34,7 @@ class SecureFileIO(object):
 
     @classmethod
     def update_url_data(cls,url,hashed_uuid,public_key,shared_key):
+        """updates the web data stored in the web.data file"""
         web_data = cls.load_web_data()
         web_data[url] = {
             'hashed_uuid':hashed_uuid,
@@ -40,16 +45,19 @@ class SecureFileIO(object):
 
     @classmethod
     def load_url_data(cls,url):
+        """loads the specific url data from the web.data file"""
         web_data = cls.load_web_data()
         return web_data.get(url,None)
         
         
     @classmethod
-    def save_usb_data(cls,web_data):
-        web_data = SecTools.serialize(web_data)
-        ConsoleTools.file_write('box/usb.data',web_data)
+    def save_usb_data(cls,usb_data):
+        """saves the usb data to usb.data file"""
+        usb_data = SecTools.serialize(usb_data)
+        ConsoleTools.file_write('box/usb.data',usb_data)
 
     @classmethod
     def load_usb_data(cls):        
-        web_data = ConsoleTools.file_read('box/usb.data')
-        return SecTools.deserialize(web_data) if web_data else {}
+        """loads the usb data from usb.data file"""
+        usb_data = ConsoleTools.file_read('box/usb.data')
+        return SecTools.deserialize(usb_data) if usb_data else {}
