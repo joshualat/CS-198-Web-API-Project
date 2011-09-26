91e275d12a2e93114c32e1843e8d78055de084d7
kcvajgf <kevin.charles.atienza@gmail.com>
2011-09-26 17:27:21 +0800

    with user readme

---
+++ USB_PR~1/create.py
@@ -0,0 +1,41 @@
+from lib.ConsoleTools import *
+from help import *
+from util import *
+from gen_crypt_data import *
+import shutil
+from info import *
+
+@with_help('create')
+def create_usb(off=1):
+    ''' 
+    creates usb account: formats usb drive, registers it to main site, and gathers data. 
+    this code may be run anywhere.
+    '''
+    
+    confirm = ConsoleTools.accept_input('WARNING: ALL data from USB will be deleted. Continue?',['y','n'])
+    if confirm == 'y':
+        password = input_password()
+        set_password(password)
+        
+        print 'Formatting usb...'
+        path=ConsoleTools.format_usb(password)
+        print 'Opening usb...'
+        diskpath=ConsoleTools.read_usb(path,password)
+        if not (path and diskpath):
+            raise Error()
+        try:
+            diskpath += 'crypt_data/'
+            print 'Copying files to', diskpath, '...'
+            shutil.copytree('.', diskpath)
+            os.makedirs(diskpath + 'box')
+            gen_crypt_data(path=diskpath)
+            print 'Editing information...'
+            edit_info(path=diskpath)
+            print 'New USB account successfully created.'
+        finally:
+            ConsoleTools.close_usb(path,diskpath,password)
+    else:
+        print 'No files created.'
+    
+if __name__ == "__main__":
+    create_usb()
---
+++ USB_PR~1/lib/SecTools.py
@@ -0,0 +1,65 @@
+import uuid
+import hashlib
+import cPickle as pickle
+from Crypto.Random import random
+
+# SecTools (Security Tools) Class
+
+class SecTools(object):
+
+    """
+    SecTools class 
+
+    Usable functions:
+        generate_salt
+        generate_uuid
+        generate_hash
+        verify_hash
+        random_int
+        random_choice
+        random_float
+        serialize
+        deserialize
+
+    """
+
+    @classmethod
+    def generate_salt(cls,length=16):
+        ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        return ''.join(cls.random_choice(ALPHABET) for i in range(length))
+
+    @classmethod
+    def generate_uuid(cls):
+        return str(uuid.uuid1())
+
+    @classmethod
+    def generate_hash(cls,obj,salt=None):
+        obj = cls.serialize(obj)
+        if salt:
+            obj = obj + salt
+        return hashlib.sha256(obj).digest()
+
+    @classmethod
+    def verify_hash(cls,sig,obj,salt=None):
+        return sig == cls.generate_hash(obj,salt)
+
+    @classmethod
+    def random_int(cls,a,b):
+        return random.randint(a,b)
+
+    @classmethod
+    def random_choice(cls,seq):
+        return random.choice(seq)
+
+    @classmethod
+    def random_float(cls):
+        rand_int = random.randint(0,9999999999)
+        return float(rand_int)/float(10000000000)
+
+    @classmethod
+    def serialize(cls,obj):
+        return pickle.dumps(obj)
+
+    @classmethod
+    def deserialize(cls,obj):
+        return pickle.loads(obj)
